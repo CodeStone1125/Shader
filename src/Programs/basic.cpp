09e96832deb77c418ebf18c7f215e341202685cb
Max-Chu1125 <williechu1125@gmail.com>
2023-11-25 03:59:29 +0800

    UNDONE: TODO#3: Plane texture only draw the top(90%)

--- src/Programs/basic.cpp
+++ src/Programs/basic.cpp
@@ -31,8 +31,9 @@ bool BasicProgram::load() {
     Model* model = ctx->models[i];
 
     GLuint VBO[3];
+    // I generate 3 VBO to store position, normal, texcoord respectively .
+    // It's different way compared to https://learnopengl.com/Getting-started/Textures
     glGenBuffers(3, VBO);
-
     glBindBuffer(GL_ARRAY_BUFFER, VBO[0]);
     glBufferData(GL_ARRAY_BUFFER, sizeof(float) * model->positions.size(), model->positions.data(), GL_STATIC_DRAW);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (void*)0);
